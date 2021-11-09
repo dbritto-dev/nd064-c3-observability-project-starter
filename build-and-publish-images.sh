64a5bf8b5d22ebfc755e85da794e542c657973cb
dbritto-dev <dbritto.dev@gmail.com>
2021-11-09 00:37:25 -0500

    update scripts

--- build-and-publish-images.sh
+++ build-and-publish-images.sh
@@ -4,10 +4,32 @@ DOCKER_USER=minorpatch
 
 docker login
 
-docker build -f ./reference-app/backend/Dockerfile ./reference-app/backend -t $DOCKER_USER/metrics-dashboard-backend
-docker build -f ./reference-app/frontend/Dockerfile ./reference-app/frontend -t $DOCKER_USER/metrics-dashboard-frontend
-docker build -f ./reference-app/trial/Dockerfile ./reference-app/trial -t $DOCKER_USER/metrics-dashboard-trial
+echo ""
+echo "Docker build and publish images for backend, frontend and trial"
+echo "==========================="
 
+echo ""
+echo -ne "Build and publish image for backend [ ]" 
+docker build -f ./reference-app/backend/Dockerfile ./reference-app/backend -t $DOCKER_USER/metrics-dashboard-backend
 docker push $DOCKER_USER/metrics-dashboard-backend
+sleep .5
+echo -ne "\rBuild and publish image for backend [✓]\n"
+
+echo ""
+echo -ne "Build and publish image for frontend [ ]" 
+docker build -f ./reference-app/frontend/Dockerfile ./reference-app/frontend -t $DOCKER_USER/metrics-dashboard-frontend
 docker push $DOCKER_USER/metrics-dashboard-frontend
+sleep .5
+echo -ne "\rBuild and publish image for frontend [✓]\n"
+
+echo ""
+echo -ne "Build and publish image for trial [ ]" 
+docker build -f ./reference-app/trial/Dockerfile ./reference-app/trial -t $DOCKER_USER/metrics-dashboard-trial
 docker push $DOCKER_USER/metrics-dashboard-trial
+sleep .5
+echo -ne "\rBuild and publish image for trial [✓]\n"
+
+echo ""
+echo "Done"
+
+exit 0
